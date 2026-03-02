9ab5dfeea17d26d5d3a5057c8222ad62658866f9
Natalia Rolon <nataliarolonleal@gmail.com>
2026-03-02 17:15:43 -0500

    docs: actualizar readme con instrucciones

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ print("---------")
 print("Ingrese el primer número")
 num1 = int(input(":"))
 
+print("Ingrese el segundo número")
+num2 = int(input(":"))
+
+suma_numeros = num1 + num2
+
+print("La suma de los dos números es: ",suma_numeros)
